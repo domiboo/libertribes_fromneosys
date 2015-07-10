675bd4c54921ccc4980cbb70776cfa0a9c4058fb
Chris Fromont <Chris Fromont@localhost>
2015-07-10 16:13:41 +0200

    Modification de la police (suite)

--- hegoa_2015/contenus/page_connexion.php
+++ hegoa_2015/contenus/page_connexion.php
@@ -6,22 +6,22 @@
 <?php
 if(isset($_GET['erreur'])&&$_GET['erreur']=='1'){
 ?>
-<p style="text-align: center;font-family:kleymisskyregular;font-size:26px;font-weight: bold;color:#330000;margin:0;"><b>Le formulaire n'est pas complet. Réessayez.</b></p>
+<p style="text-align: center;font-family:bilbo_swash_capsregular;font-size:26px;font-weight: bold;color:#330000;margin:0;"><b>Le formulaire n'est pas complet. Réessayez.</b></p>
 <?php
 }
 elseif(isset($_GET['erreur'])&&$_GET['erreur']=='2'){
 ?>
-<p style="text-align: center;font-family:kleymisskyregular;font-size:26px;font-weight: bold;color:#330000;margin:0;"><b>Le token de sécurité est incorrect. Réessayez.</b></p>
+<p style="text-align: center;font-family:bilbo_swash_capsregular;font-size:26px;font-weight: bold;color:#330000;margin:0;"><b>Le token de sécurité est incorrect. Réessayez.</b></p>
 <?php
 }
 elseif(isset($_GET['erreur'])&&$_GET['erreur']=='10'){
 ?>
-<p style="text-align: center;font-family:kleymisskyregular;font-size:26px;font-weight: bold;color:#330000;margin:0;"><b>Soit le compte est en attente de validation, soit le compte n'existe pas et il faut alors vous inscrire, soit les données introduites sont incorrectes et il faut réessayez.</b></p>
+<p style="text-align: center;font-family:bilbo_swash_capsregular;font-size:26px;font-weight: bold;color:#330000;margin:0;"><b>Soit le compte est en attente de validation, soit le compte n'existe pas et il faut alors vous inscrire, soit les données introduites sont incorrectes et il faut réessayez.</b></p>
 <?php
 }
 elseif(isset($message)&&!empty($message)){
 ?>
-<p style="text-align: center;font-family:kleymisskyregular;font-size:26px;font-weight: bold;color:#330000;margin:0;"><b><?php echo $message; ?></b></p>
+<p style="text-align: center;font-family:bilbo_swash_capsregular;font-size:26px;font-weight: bold;color:#330000;margin:0;"><b><?php echo $message; ?></b></p>
 <?php
 }
 else {
